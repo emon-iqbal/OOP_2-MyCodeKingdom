e2cf1905232a9c2d74256ae60d034c2347d4c66a
emon-iqbal <rasjbii@gmail.com>
2021-02-02 19:14:05 +0600

    ENVIRONMENT SETUP+BASIC PROGRAM TO SHOW OUTPUT
    startup of visual studio 2019 and writing simple program using the syntax  "Console.WriteLine" to show the ultimate output in console window

---
+++ LABTASK_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CV__EMON_RASEL_IQBAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                             ---------------------------------------   ");
+            Console.WriteLine("                                         PERSONAL INFORMATIONS         ");
+            Console.WriteLine("                          ---------------------------------------   \n ");
+            Console.WriteLine("         1.  NAME:               EMON, RASEL IQBAL      ");
+            Console.WriteLine("         2.  DESIGNATION:        Student                ");
+            Console.WriteLine("         3.  email:              ras.iqbal.97@gmail.com ");
+            Console.WriteLine("         4.  DATE OF BIRTH:      20 NOV 1997            ");
+            Console.WriteLine("         5.  BLOODGROUP:         O +ve                  ");
+            Console.WriteLine("         6.  NATIONALITY:        Bangladeshi            ");
+            Console.WriteLine("         7.  GENDER:             MALE                   ");
+            Console.WriteLine("         8.  RELIGION:           ISLAM \n               ");
+            
+            Console.WriteLine("         9.  ------------        EDUCATION:      ------------ ");
+            Console.WriteLine("                 SSC:      ADAMJEE CANTONMENT PUBLIC SCHOOL, DHAKA               [[2013, 5.00]] ");
+            Console.WriteLine("                 HSC:      ADAMJEE CANTONMENT COLLEGE, DHAKA                     [[2015, 4.96]] ");
+            Console.WriteLine("                 BSC.CSE:  AMERICAN INTERNATIONAL UNIVERSITY- BANGLADESH, DHAKA  [[~~~S T U D Y I N G~~~]] \n ");
+
+            Console.WriteLine("         10. ------------        EXPERIENCES:    ------------ \n ");
+            Console.WriteLine("              MINI PROJECTS::   ");
+            Console.WriteLine("                 WEB TECHNOLOGY:                           ONLINE TUITION MEDIA          [[2020, May]] ");
+            Console.WriteLine("                 JAVA [Object Oriented Programming # 1]:   PHARMACY MANAGEMENT SYSTEM    [[2019, Aug]] ");
+            Console.WriteLine("              PRESENTATIONS::   ");
+            Console.WriteLine("                 Computer Networks, Software Engineering, Object Oriented Analysis and Design, ");
+            Console.WriteLine("                 Introduction to Database, Engineering Management \n ");
+
+            Console.WriteLine("         11. SKILLS:         Web development, Computer Networking, Dynamic Routing, Materialize CSS, ");
+            Console.WriteLine("                         HTML, JavaScript, pHp, NODEJS, LARAVEL \n ");
+            
+            Console.WriteLine("         12. NAME of FATHER:         MUHAMMAD IDRISH IQBAL ");
+            Console.WriteLine("         13. ->  DESIGNATION:        Businessman           ");
+            Console.WriteLine("         14. NAME of MOTHER:         RASHIDA IQBAL         ");
+            Console.WriteLine("         15. ->  DESIGNATION:        Housewife             ");
+            Console.WriteLine("         16. PERMANENT ADDRESS:      SALTHA, FARIDPUR      ");
+            Console.WriteLine("         17. PRESENT ADDRESS:        Eskaton Garden Road, Ramna-1000, DHAKA ");
+            
+            Console.WriteLine("         18. ACHIEVEMENTS:           ACPS Seasonal Quiz Contest: 3rd Prize Winner [[2014]], ");
+            Console.WriteLine("                                     ACPS Inter Debate Program: Runner-up [[2011]] \n       ");
+            
+            Console.WriteLine("         19. INTERESTS IN:           Music, Cricket, Football, Chess, Billiards   ");
+            Console.WriteLine("         20. AIM IN LIFE:            To be a positive and renowned person forever ");
+
+            Console.ReadKey();
+
+        }
+    }
+}

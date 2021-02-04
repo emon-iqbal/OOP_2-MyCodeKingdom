91f82e3daea5477bc228517d6842061f9de5e450
emon-iqbal <rasjbii@gmail.com>
2021-02-04 07:02:47 +0600

    SWAP / EXCHANGE TWO INTEGERS
    input:  A = something,
               B  = other.......
    
    output:   A  =  other.......
                   B  =  something,

---
+++ TASK_3/3_Exchange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TASK_3_3
+{
+    class Exchange
+    {
+        static void Main(string[] args)
+        {
+            int A, B, SWAP;
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("     --------------- EXCHANGE TWO INTEGERS ----------    ");
+            Console.WriteLine("     ------------------------------------------------    ");
+
+
+            Console.Write(Environment.NewLine + "     DROP 1st INTEGER HERE:	A	=	  ");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("     DROP 2nd INTEGER HERE:	B	=	  ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Environment.NewLine + "       >>>USER INPUT WAS :>>      ~ A ~  =   " + A);
+            Console.WriteLine("                                  ~ B ~  =   " + B);
+
+            SWAP = A;
+            A = B;
+            B = SWAP;
+
+            Console.WriteLine(Environment.NewLine + "       >>>ULTIMATE OUTPUT IS :>>  ~ A ~  =   " + A);
+            Console.WriteLine("                                  ~ B ~  =   " + B);
+
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+
+    }
+}

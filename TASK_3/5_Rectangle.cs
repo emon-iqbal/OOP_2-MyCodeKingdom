216de2678613b110259ed62288c5ed8659a3a177
emon-iqbal <rasjbii@gmail.com>
2021-02-04 06:11:58 +0600

    TASK_3
    1. Grades of a student,
    2. Total Cost of a product,
    3. Swapping two integers;
    4. Calculation of ultimate Average for multiple integers,
    5. Calculation of area and perimeter of a rectangle...

---
+++ TASK_3/5_Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TASK_3_5
+{
+    class Rectangle
+
+    {
+        static void Main(string[] args)
+        {
+
+            decimal W, H;
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("     --------------- CALCULATE AREA AND PERiMETER OF A RECTANGLE ---------- ");
+            Console.WriteLine("     ---------------------------------------------------------------------- ");
+            
+            Console.WriteLine(Environment.NewLine);
+            
+            Console.Write("     WiDTH OF A RECTANGLE : >>      ");
+            W = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("     HEiGHT OF A RECTANGLE: >>      ");
+            H = Convert.ToDecimal(Console.ReadLine());
+            
+            decimal AREA = W * H;
+            decimal PER  = 2 * (W + H);
+
+            Console.WriteLine(Environment.NewLine);
+            
+            Console.WriteLine("     AREA OF THE RECTANGLE       =   "  +  AREA );
+            Console.WriteLine("     PERiMETER OF THE RECTANGLE  =   "  +  PER  );
+
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+    }
+}

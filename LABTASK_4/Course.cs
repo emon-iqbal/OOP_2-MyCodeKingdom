3c273a62774af2c6fe871a1916d82be3d69e2c73
emon-iqbal <rasjbii@gmail.com>
2021-02-10 15:55:51 +0600

    3.  COURSE # Project:: COURSE || class:: Course
    .......developing the followings under the class named as Course::
    ----------------------------------------------------------------------
    1.  Course_Name
    2.  Course_Code
    4.  Course_Credit

---
+++ LABTASK_4/Course.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COURSE
+{
+    class Course
+    {
+        private string courseName;
+        private string courseCode;
+        private int courseCredit;
+
+
+        private void SetCourse(string COURSE_NAME, string COURSE_CODE, int COURSE_CREDIT)
+        {
+            courseName = COURSE_NAME;
+            courseCode = COURSE_CODE;
+            courseCredit = COURSE_CREDIT;
+        }
+
+        private string GetcourseName()
+        {
+            return courseName;
+        }
+        
+        private string GetcourseCode()
+        {
+            return courseCode;
+        }
+
+        private int GetcourseCredit()
+        {
+            return courseCredit;
+        }
+
+        
+        
+        private void ShowCourseInfo()
+        {
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("  >>>>> S E L E C T E D      C O U R S E      N A M E                          ###   " + GetcourseName());
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("  >>>>> C O D E      O F      T H E      S P E C i F i E D      C O U R S E    ###   " + GetcourseCode());
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("  >>>>> T O T A L      C R E D i T      F O R      T H E      C O U R S E      ###   " + GetcourseCredit());
+
+        }
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Course oop2 = new Course();
+            oop2.SetCourse("W E B   T E C H N O L O G Y", "CSC3222", 3);
+            oop2.ShowCourseInfo();
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
